a23b49513e927a0c8b3ff99c1751d52bc397f4a5
NadunKavindra <nadun.kavindra@gmail.com>
2017-01-01 21:40:26 +0530

    Change listen

--- Express_REST/app.js
+++ Express_REST/app.js
@@ -9,4 +9,11 @@ app.get('/Home', function (req, res) {
     res.json({ message: 'hooray! welcome Home!' });
 });
 
-app.listen(process.env.PORT || 7001);
+var server = app.listen(7001, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+//app.listen(process.env.PORT || 7001);
